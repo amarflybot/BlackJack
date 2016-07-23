b2253456f5510b8657a75d37dfc90ac26681921f
amarraja123 <amar.arawat@gmail.com>
2016-07-23 17:24:50 +0530

    Refactor Card

--- src/github.com/amarraja123/BlackJack/BlackJack.go
+++ src/github.com/amarraja123/BlackJack/BlackJack.go
@@ -1,5 +1,11 @@
-package BlackJack
+package main
 
+import (
+	"math/rand"
+	"fmt"
+	"bufio"
+	"os"
+)
 
 type BlackJack struct {
 	deck *Deck
@@ -7,38 +13,65 @@ type BlackJack struct {
 	player *Player
 }
 
-type Dealer struct {
-	score int
-}
-
-type Player struct {
-	score int
-}
-
 type Winner interface {
 
 }
 
-type Deck struct {
-	cards []string
-}
-
-func (blackjack BlackJack) CheckWin() Winner {
+func (blackjack *BlackJack) CheckWin() Winner {
 	if blackjack.player.score == 21{
 		return blackjack.player
+	} else if blackjack.player.score >= 21{
+		return blackjack.dealer
 	}
-	return blackjack.dealer
+	return nil
 }
 
-func (blackjack BlackJack) NewDeck() *Deck {
-	deck := new(Deck)
-	deck.cards = []string{"A","A","A","A",
-	"2","2","2","2",
-	"3","3","3","3",
-	"4","4","4","4",
-	"10","10","10","10",
-	"10","10","10","10",
-	"10","10","10","10",
+func (blackJack *BlackJack)PlayGame(playerChance bool)  {
+	gameOver := false;
+	for !gameOver{
+		if playerChance{
+			//Select Card from the deck
+			cardValue := blackJack.GetCard();
+			fmt.Println("Card Value of the user %s ",cardValue.name)
+			//Get Input from user
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Print("Enter HIT or STAND: ")
+			text, _ := reader.ReadString('\n')
+			if text == "HIT\n" {
+				blackJack.player.score += cardValue.value;
+				winner := blackJack.CheckWin()
+				if winner == nil {
+					continue
+				} else {
+					gameOver = true
+					fmt.Printf("Winner : %v ", winner)
+				}
+
+			} else if text == "STAND\n"{
+				playerChance = false
+				continue
+			}
+		} else {
+			//Select Card from the deck
+			cardValue := blackJack.GetCard();
+			fmt.Println("Card Value of the user %s ",cardValue.name)
+				blackJack.dealer.score += cardValue.value;
+				winner := blackJack.CheckWin()
+				if winner == nil {
+					continue
+				} else {
+					gameOver = true
+					fmt.Printf("Winner : %v " , winner)
+				}
+		}
 	}
-	return deck
+}
+
+
+func (blackjack *BlackJack) GetCard() Card {
+	sliceIndex := rand.Intn(len(blackjack.deck.cards))
+	randomCard := blackjack.deck.cards[sliceIndex]
+	//remove card from slice after selection
+	blackjack.deck.cards = append(blackjack.deck.cards[:sliceIndex], blackjack.deck.cards[sliceIndex+1:]...)
+	return randomCard
 }
