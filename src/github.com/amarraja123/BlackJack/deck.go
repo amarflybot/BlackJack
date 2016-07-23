c7a760cad09386bf6c50d92ff78a050c38fba82f
amarraja123 <amar.arawat@gmail.com>
2016-07-23 16:47:59 +0530

    Refactor

--- src/github.com/amarraja123/BlackJack/deck.go
+++ src/github.com/amarraja123/BlackJack/deck.go
@@ -7,4 +7,19 @@ type Deck struct {
 type Card struct {
 	value int
 	altValue int
+	name string
+}
+
+
+
+func (blackjack *BlackJack) NewDeck() *Deck {
+	blackjack.deck = new(Deck)
+
+	card := new(Card)
+	card.value = 1;
+	card.altValue = 11;
+	card.name = "A"
+	blackjack.deck.cards = append(blackjack.deck.cards,card)
+
+	return blackjack.deck
 }
